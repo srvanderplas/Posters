e13abb79d0d0a686ad0ece2dea48194924b5d88a
srvander <srvanderplas@gmail.com>
2019-02-13 10:49:51 -0600

    Use film prints instead of two sets of 2d scans

--- 201902_AAFS_ShoeDB/convert_tiff_pdf.sh
+++ 201902_AAFS_ShoeDB/convert_tiff_pdf.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 for f in DLData/*.tiff; do
   a=$(basename -s .tiff $f);
   b=$(dirname $f);
   tiff2pdf -j -q 99 -x 300 -y 300 -f -o "$b/$a.pdf" $f;
+done;
+
+for f in DLData/*.tif; do
+  a=$(basename -s .tif $f);
+  b=$(dirname $f);
+  convert "$b/$a.tif" -units PixelsPerInch image -density 300 -compress JPEG -quality 99 "$b/$a.pdf" $f;
 done;
